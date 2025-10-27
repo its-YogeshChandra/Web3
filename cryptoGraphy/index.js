417f7858f50a02ca07c33150cfff37f1b729599c
LG-Crusty <pandittheroyal@gmail.com>
2025-10-27 22:55:34 +0530

    added a simple model of asymmetric cryptography

--- cryptoGraphy/index.js
+++ cryptoGraphy/index.js
@@ -1,8 +1,38 @@
-import solana from "@solana/web3.js";
-const genratorfunc = async () => {
-  //generate the key
-  const keys = new solana.Keypair();
-  console.log("keys:" + keys);
+import nacl from "tweetnacl";
+import { Keypair } from "@solana/web3.js";
+
+const keypairGen = () => {
+  //generate keypair
+  const key = Keypair.generate();
+
+  const publickey = key.publicKey;
+  const privatekey = key.secretKey;
+
+  return {
+    publickey,
+    privatekey,
+  };
 };
 
-genratorfunc();
+const signtheArray = () => {
+  const { publickey, privatekey } = keypairGen();
+  console.log(`publickey: ${publickey.toString()}`);
+  console.log(`privatekey: ${privatekey}`);
+
+  const message = "hello world";
+  const encodedMessage = new TextEncoder().encode(message);
+  
+  //creating the signature
+  const signature = nacl.sign.detached(encodedMessage, privatekey);
+
+  const result = nacl.sign.detached.verify(
+    encodedMessage,
+    signature,
+    publickey.toBytes()
+  );
+
+  console.log(result);
+};
+
+signtheArray();
+
