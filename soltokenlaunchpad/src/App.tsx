92cea9a962bcc616e073551a46ae622e84767ecb
its-YogeshChandra <pandittheroyal@gmail.com>
2025-11-18 13:27:54 +0530

    adding routing, and other important packages
    adeed important packages, shadcn , router, tailwind and so on

--- soltokenlaunchpad/src/App.tsx
+++ soltokenlaunchpad/src/App.tsx
@@ -1,34 +1,43 @@
-import './App.css'
+// wallet adapter imports
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import {
   WalletModalProvider,
-  WalletDisconnectButton,
-  WalletMultiButton
 } from '@solana/wallet-adapter-react-ui';
 import '@solana/wallet-adapter-react-ui/styles.css';
+import { Outlet } from 'react-router';
+import { Toaster } from 'sonner';
+
+
 
 function App() {
-
   return (
-    <>
-      <div>
-        <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
-          <WalletProvider wallets={[]} autoConnect>
-            <WalletModalProvider>
-              <div style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                padding: 20
-              }}>
-                <WalletMultiButton />
-                <WalletDisconnectButton />
-              </div>
-            </WalletModalProvider>
-          </WalletProvider>
-        </ConnectionProvider>
-      </div>
-    </>
+    <div>
+      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+        <WalletProvider wallets={[]} autoConnect>
+          <WalletModalProvider>
+            <div style={{
+              padding: 20
+            }}>
+              <Outlet />
+              <Toaster
+                position="bottom-left"
+                theme="light"
+                toastOptions={{
+                  duration: 3000,
+                  style: {
+                    background: "#111",
+                    color: "#fff",
+                    borderRadius: "8px",
+                  },
+                }}
+              />
+
+            </div>
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
   )
 }
 
-export default App
+export default App;
---
+++ soltokenlaunchpad/src/routes/routes.ts
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router";
+import App from "../App.tsx";
+
+
+const Routing = createBrowserRouter([
+  {
+    path: "/",
+    Component: App
+  }
+
+
+])
+
+export { Routing };
