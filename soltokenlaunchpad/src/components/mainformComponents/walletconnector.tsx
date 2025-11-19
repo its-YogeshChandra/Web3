864a405d430545d9434a73fb1673ea4587d655be
its-YogeshChandra <pandittheroyal@gmail.com>
2025-11-19 21:03:37 +0530

    saving commit

--- soltokenlaunchpad/src/components/mainformComponents/walletconnector.tsx
+++ soltokenlaunchpad/src/components/mainformComponents/walletconnector.tsx
@@ -1,9 +1,17 @@
+import {
+  WalletDisconnectButton,
+  WalletMultiButton
+} from "@solana/wallet-adapter-react-ui"
+
 function WalletConnector() {
   return (
-    <div className="w-full h-40 bg-emerald-500 mt-10">
+    <div className="w-full h-40 bg-emerald-500 mt-10 flex items-center">
 
       {/* wallet connection */}
 
+
+      <WalletMultiButton />
+      <WalletDisconnectButton />
 
     </div>
 
