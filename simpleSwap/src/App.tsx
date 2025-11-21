24b2fe2ea9272b65a9fed32ffb59925393d119f2
its-YogeshChandra <pandittheroyal@gmail.com>
2025-11-21 17:46:00 +0530

    added simpleswap
    dummy project to learn about swapping

---
+++ simpleSwap/src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <div className='w-screen h-screen bg-slate-800 flex flex-col items-center justify-center ' >
+        <div className='w-[570px] h-[420px] bg-[#680747] p-2 flex flex-col gap-y-3 '>
+          <div className='w-full h-[49%] bg-fuchsia-400 p-2 flex flex-col items-center justify-center'>
+            <div className='w-full h-max flex justify-between px-8  '>
+              <p className='text-3xl'>From</p>
+              <p className='text-xl'>Balance</p>
+            </div>
+            <div className='w-full h-max flex px-8 justify-between  '>
+              <p className='text-xl'>SOL</p>
+              <input type='number'
+                className='bg-white w-30'
+              />
+            </div>
+          </div>
+          <div className='w-full h-[50%] bg-fuchsia-400  p-2 flex flex-col items-center justify-center'>
+            <div className='w-full h-max flex justify-between px-8  '>
+              <p className='text-3xl'>From</p>
+              <p className='text-xl'>Balance</p>
+            </div>
+            <div className='w-full h-max flex px-8 justify-between  '>
+              <p className='text-xl'>USDC</p>
+              <input type='number'
+                className='bg-white w-30'
+              />
+            </div>
+
+          </div>
+        </div>
+        <div className='w-[570px] h-24 bg-amber-600 rounded-2xl flex justify-center items-center '>
+          <button className="text-2xl"> SWAP</button>
+        </div>
+      </div >
+    </>
+  )
+}
+
+export default App
---
+++ simpleSwap/src/services/SwapService.ts
@@ -0,0 +1,22 @@
+import { Keypair, Connection } from "@solana/web3.js";
+import { base58 } from "@scure/base";
+
+export class swapfunctions {
+  private generateKeyPair(privatekey: string): Keypair {
+    const neededKey = base58.decode(privatekey)
+    const keypair = Keypair.fromSecretKey(neededKey)
+    return keypair
+
+  }
+  private keyPair
+  private connection
+  constructor(devnetUrl: string, privateKey: string) {
+    this.keyPair = this.generateKeyPair(privateKey)
+    this.connection = new Connection(devnetUrl)
+  }
+
+}
+
+const swapServices = new swapfunctions();
+
+export { swapServices }
