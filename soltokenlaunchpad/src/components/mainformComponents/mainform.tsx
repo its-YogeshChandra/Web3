156941cf0b2d1170306cf92f2914e3b9b75ae36a
its-YogeshChandra <pandittheroyal@gmail.com>
2025-11-27 00:59:31 +0530

    starting creating the form model

---
+++ soltokenlaunchpad/src/components/mainformComponents/mainform.tsx
@@ -0,0 +1,15 @@
+import { useForm } from "react-hook-form"
+
+
+
+function MainForm() {
+
+  return (
+    <div>
+
+    </div>
+  )
+
+}
+
+export default MainForm
--- soltokenlaunchpad/src/components/mainformComponents/walletconnector.tsx
+++ soltokenlaunchpad/src/components/mainformComponents/walletconnector.tsx
@@ -2,19 +2,32 @@ import {
   WalletDisconnectButton,
   WalletMultiButton
 } from "@solana/wallet-adapter-react-ui"
-
 function WalletConnector() {
+
+  // const checkAddress = (operation: string) => {
+  //   //check if the connecton to wallet is true and public address is not empty or undefined 
+  //   if (operation == "setWallet") {
+  //     const connection = wallet.connected;
+  //     if (connection == true) {
+  //       setiswalletopen(prev => !prev)
+  //     }
+  //   }
+  //   else {
+  //     if (wallet.connected == false) {
+  //       setiswalletopen(prev => !prev)
+  //     }
+  //   }
+  // }
+  //
+
   return (
-    <div className="w-full h-40 bg-emerald-500 mt-10 flex items-center">
+    <div className="w-full h-max py-10 mt-10 border-2 border-slate-500flex items-center gap-x-3">
 
       {/* wallet connection */}
-
-
-      <WalletMultiButton />
-      <WalletDisconnectButton />
-
-    </div>
-
+      <div className="w-full h-max pl-10 flex gap-x-3"> < WalletMultiButton />
+        < WalletDisconnectButton />
+      </div>
+    </div >
   )
 }
 
