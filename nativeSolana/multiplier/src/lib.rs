b944187749079a11a8059f4b4178720d41c3b425
its-YogeshChandra <pandittheroyal@gmail.com>
2025-12-03 01:27:27 +0530

    multiplier main function is written
    testing of the function is remaining

--- nativeSolana/multiplier/src/lib.rs
+++ nativeSolana/multiplier/src/lib.rs
@@ -14,7 +14,8 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::{Account, AccountInfo, next_account_info},
-    entrypoint::{self, ProgramResult},
+    entrypoint,
+    entrypoint::ProgramResult,
     msg,
     pubkey::Pubkey,
 };
@@ -41,13 +42,20 @@ pub fn multiplier_function(
     let acc = next_account_info(&mut accounts.iter()).expect("failed to iterate over the array");
     //use of borsh
     let instruction_type = InstructionType::try_from_slice(instruction_data)?;
+
+    //adding to the system
+    let mut data = MultipliedValue::try_from_slice(&mut acc.data.borrow())?;
+
     match instruction_type {
         InstructionType::Number(value) => {
-            let mut data = MultipliedValue::try_from_slice(&mut acc.data.borrow())?;
             data.input = [value[0] as i32, value[1] as i32];
             data.result = (value[0] * value[1]) as i32;
         }
     }
+    data.serialize(&mut *acc.data.borrow_mut())
+        .expect("error while serealizing the data ");
+
+    // add the data into the system
 
     Ok(())
 }
